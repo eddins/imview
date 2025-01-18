918cd060ab359d6f9701c5dc58435e38cb3a190f
eddins <4043689+eddins@users.noreply.github.com>
2025-01-18 12:36:15 -0500

    Add tests for Parent argument

---
+++ tests/imview_parent_test.m
@@ -0,0 +1,15 @@
+classdef imview_parent_test < imview_test_setup
+    methods (Test)
+        function specifyParent_test(test_case)
+            tl = tiledlayout("flow",Parent = test_case.Figure);
+            ax1 = nexttile(tl);
+            ax2 = nexttile(tl);
+
+            im1 = imview(1, Parent = ax1);
+            im2 = imview(1, Parent = ax2);
+
+            test_case.verifyEqual(im1.Parent, ax1);
+            test_case.verifyEqual(im2.Parent, ax2);
+        end
+    end
+end
